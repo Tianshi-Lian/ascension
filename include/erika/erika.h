f9a1d1789636996c66c57d4681540f807a0f4b26
Rob Graham <r.ggraham@hotmail.com>
2023-02-09 21:15:33 +0000

    feat(build): Setup project structure

---
+++ include/erika/erika.h
@@ -0,0 +1,4 @@
+
+namespace erika {
+    int test();
+}
---
+++ src/erika.cpp
@@ -0,0 +1,8 @@
+#include "erika.h"
+
+#include <iostream>
+
+int erika::test() {
+    std::cout << "void erika::test()" << std::endl;
+    return 0;
+}

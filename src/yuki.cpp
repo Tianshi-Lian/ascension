87db6fcfb01997ba6bbab969e81bb7a7576ec765
Rob Graham <r.ggraham@hotmail.com>
2023-02-19 21:24:44 +0000

    defeat(core): Remove logger initialization from library

--- src/yuki.cpp
+++ src/yuki.cpp
@@ -3,10 +3,6 @@
 #include "debug/logger.h"
 
 int yuki::test() {
-    Logger::initialize("logs/app.txt");
-    Logger::set_log_severity_level(Log_Level::DEBUG);
-    Logger::enable_console_logging(true);
-
     Logger::info("void yuki::test()");
 
     return 2;

0790e3323f4b1fe2da8f98fa271acb7f9eab8290
Rob Graham <r.ggraham@hotmail.com>
2023-02-19 14:29:30 +0000

    refactor(core): Move over to yuki logger

--- ascension/src/main.cpp
+++ ascension/src/main.cpp
@@ -3,13 +3,15 @@
 #include <erika/erika.h>
 #include <yuki/yuki.h>
 
+#include <yuki/debug/logger.h>
+
 int main() {
+    yuki::Logger::info("Initializing Ascension...");
 
     i32 e_result = erika::test();
     i32 y_result = yuki::test();
 
-    std::cout << "erika::test: " << e_result << "\n"
-              << "yuki::test: " << y_result << std::endl;
+    yuki::Logger::notice("erika::test: %d, yuki::test: %d", e_result, y_result);
 
     return 0;
 }

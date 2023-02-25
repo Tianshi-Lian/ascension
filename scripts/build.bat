126cfde8725dbc29861aadd74d0d6d086e07cf97
Rob Graham <r.ggraham@hotmail.com>
2023-02-25 11:43:43 +0000

    refactor(build): Unify build styles, remove main exe from root cmake

--- scripts/build.bat
+++ scripts/build.bat
@@ -2,10 +2,6 @@
 
 SET build_args=-j 16
 
-if not exist "..\build\dist\" (
-    mkdir ..\build\dist
-)
-
 if not exist "..\build\build.ninja" (
     cmake -S ..\ -B ..\build -G "Ninja" -D CMAKE_CXX_COMPILER=clang++
 )
@@ -14,6 +10,6 @@ pushd ..\build
 ninja %build_args%
 popd
 
-if not exist "..\build\dist\logs" (
-    mkdir ..\build\dist\logs
+if not exist "..\build\ascension\bin\logs" (
+    mkdir ..\build\ascension\bin\logs
 )

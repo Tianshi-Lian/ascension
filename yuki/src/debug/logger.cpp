f82ceacc924219727dc63adf508179acb8538039
Rob Graham <r.ggraham@hotmail.com>
2023-02-26 15:08:26 +0000

    refactor(yuki/debug): Modernise logger & remove unused error codes

--- yuki/src/debug/logger.cpp
+++ yuki/src/debug/logger.cpp
@@ -1,11 +1,7 @@
 #include "debug/logger.hpp"
 
-#include <array>
-#include <ctime>
+#include <filesystem>
 #include <iostream>
-#include <unistd.h>
-
-#include <magic_enum/magic_enum.hpp>
 
 using namespace std;
 using namespace yuki;
@@ -14,15 +10,13 @@ using namespace std::chrono;
 namespace {
 
 template <typename... Args>
-void write_direct_log(const char* format, Args&&... args) {
-    std::array<char, MAX_LEN_FMT_BUFFER> format_buffer{0};
-#pragma GCC diagnostic ignored "-Wformat-nonliteral"
-#pragma GCC diagnostic ignored "-Wformat-security"
-    int ret = snprintf(format_buffer.data(), MAX_LEN_FMT_BUFFER, format, std::forward<Args>(args)...);   // NOLINT
+void write_direct_log(const std::string& format, Args&&... args) {
+    std::vector<char> format_buffer(128);
+    int ret = snprintf(format_buffer.data(), format_buffer.size(), format.c_str(), std::forward<Args>(args)...);   // NOLINT
 
     if (ret != -1) {
-        std::array<char, MAX_LEN_STR_BUFFER> string_buffer{0};
-        ret = snprintf(string_buffer.data(), MAX_LEN_STR_BUFFER, "%s", format_buffer.data());   // NOLINT
+        std::vector<char> string_buffer(128);
+        ret = snprintf(string_buffer.data(), string_buffer.size(), "%s", format_buffer.data());   // NOLINT
         if (ret > 0) {
             string str(string_buffer.data());
             std::cout << str << std::endl;
@@ -37,21 +31,7 @@ namespace yuki {
 Logger_Worker Logger::s_worker{};              // NOLINT
 std::mutex Logger_Util::m_localtime_mutex{};   // NOLINT
 
-int Logger_Util::file_exists(const std::string& file_path) {
-    if (access(file_path.c_str(), F_OK) != 0) {
-        if (errno == ENOENT) {
-            write_direct_log("LoggerUtil::HasPermissions() the path(%s) does not exist", file_path.c_str());
-        } else if (errno == EACCES) {
-            write_direct_log("LoggerUtil::HasPermissions() the path(%s) is not accessible", file_path.c_str());
-        }
-
-        return errno;
-    }
-
-    return 0;   // success
-}
-
-void Logger_Util::get_time_string(char* buffer) {
+std::string Logger_Util::get_time_string() {
     constexpr i64 ms_in_s = 1000;
     constexpr i64 start_year = 1900;
 
@@ -63,17 +43,17 @@ void Logger_Util::get_time_string(char* buffer) {
     std::lock_guard<std::mutex> lock(m_localtime_mutex);
     auto time_info = std::localtime(&sec_since_epoch);
 
-    if (time_info != nullptr) {
-        // Format time in yyyy-MM-dd HH:mm:ss.SSS format
+    std::vector<char> time_buffer(32);
 
+    if (time_info != nullptr) {
 #ifdef _WIN32
         // NOLINTNEXTLINE
-        const auto ret = snprintf(buffer, MAX_LEN_DATE_BUFFER, "%04lld-%02d-%02d %02d:%02d:%02d.%03lld",
+        const auto ret = snprintf(time_buffer.data(), time_buffer.size(), "%04lld-%02d-%02d %02d:%02d:%02d.%03lld",
                                   start_year + time_info->tm_year, 1 + time_info->tm_mon, time_info->tm_mday,
                                   time_info->tm_hour, time_info->tm_min, time_info->tm_sec, ms_since_epoch % ms_in_s);
 #elif __linux__
         // NOLINTNEXTLINE
-        const auto ret = snprintf(buffer, MAX_LEN_DATE_BUFFER, "%04ld-%02d-%02d %02d:%02d:%02d.%03ld",
+        const auto ret = snprintf(time_buffer.data(), time_buffer.size(), "%04ld-%02d-%02d %02d:%02d:%02d.%03ld",
                                   start_year + time_info->tm_year, 1 + time_info->tm_mon, time_info->tm_mday,
                                   time_info->tm_hour, time_info->tm_min, time_info->tm_sec, ms_since_epoch % ms_in_s);
 #endif
@@ -82,29 +62,31 @@ void Logger_Util::get_time_string(char* buffer) {
             throw Logger_Exception(Log_Exception_Type::FORMAT, "Logger_Util::get_time_string() failed to format time");
         }
     }
+
+    std::string result(time_buffer.data());
+    return result;
 }
 
-int Logger_Util::has_permissions(std::string file_path) {
+bool Logger_Util::has_permissions_for_file(const std::string& file_path) {
     // Check read access
-    if (access(file_path.c_str(), R_OK) != 0) {
-        write_direct_log("LoggerUtil::HasPermissions() the path(%s) is not readable (access denied)", file_path.c_str());
-        return errno;
+    {
+        std::ifstream file(file_path);
+        if (!file.good()) {
+            write_direct_log("LoggerUtil::HasPermissions() the path(%s) is not readable (access denied)", file_path.c_str());
+            return false;
+        }
     }
 
     // Check write access
-    if (access(file_path.c_str(), W_OK) != 0) {
-        if (errno == EACCES) {
+    {
+        std::ofstream file(file_path);
+        if (!file.good()) {
             write_direct_log("LoggerUtil::HasPermissions() the path(%s) is not write-able (access denied)", file_path.c_str());
-            return errno;
-        } else if (errno == EROFS) {
-            write_direct_log("LoggerUtil::HasPermissions() the path(%s) is not write-able (read-only file-system)",
-                             file_path.c_str());
-            return errno;
+            return false;
         }
-        return errno;
     }
 
-    return 0;
+    return true;
 }
 
 void Logger_Util::sleep(unsigned int milliseconds) {
@@ -130,7 +112,7 @@ Logger_Worker::~Logger_Worker() {
     }
 }
 
-void Logger_Worker::initialize(std::string& log_file_path) {
+void Logger_Worker::initialize(const std::string& log_file_path) {
     try {
         this->m_log_file_path = log_file_path;
 
@@ -262,8 +244,11 @@ Logger::~Logger() {
     }
 }
 
-void Logger::initialize(const std::string& log_file_path) {
+void Logger::initialize(const std::string& log_file_path, Log_Level level, bool log_to_file, bool log_to_console) {
     std::string file_path = log_file_path;
+
+    size_t final_slash = log_file_path.find_last_of('/');
+    const std::string dir_path = file_path.substr(0, final_slash);
 
     // Set application log file path to default, if empty.
     if (log_file_path.empty() || log_file_path[0] == '\0' || log_file_path[0] == ' ') {
@@ -272,37 +257,51 @@ void Logger::initialize(const std::string& log_file_path) {
                          log_file_path.c_str());
     }
 
-    // Extract path and check directory permissions
-    const std::string& dir_path = file_path;
-    size_t final_slash = dir_path.find_last_of('/');
-    std::string dir_path_value = dir_path.substr(0, final_slash);
-    const auto has_dir_permissions = Logger_Util::has_permissions(dir_path_value.c_str());
-    if (has_dir_permissions != 0) {
-        write_direct_log("Logger::initialize() failed to validate application log file directory (%s) permissions. Error (%d)",
-                         dir_path_value.c_str(), has_dir_permissions);
-        throw Logger_Exception(
-            Log_Exception_Type::PERMISSION,
-            Logger_Util::str_format(
-                "initialize::Init() failed to validate application log file directory (%s) permissions. Error (%d)",
-                dir_path_value.c_str(), has_dir_permissions));
-    }
-
-    // Check file exists, if so check permissions on that file
-    const auto file_exists = Logger_Util::file_exists(log_file_path);
-    if (file_exists == 0) {
-        const auto has_file_permissions = Logger_Util::has_permissions(log_file_path.c_str());
-        if (has_file_permissions != 0) {
-            write_direct_log("Logger::initialize() failed to validate application log file (%s) permissions. Error (%d)",
-                             log_file_path.c_str(), has_file_permissions);
+    // Check that directory exists, otherwise attempt to make it
+    if (!std::filesystem::exists(dir_path)) {
+        std::error_code error;
+        const auto success = std::filesystem::create_directory(dir_path, error);
+        if (!success) {
+            write_direct_log("Logger::initialize() failed to create log file directory (%s). Error (%d)", dir_path.c_str(),
+                             error);
             throw Logger_Exception(
-                Log_Exception_Type::PERMISSION,
-                Logger_Util::str_format(
-                    "Logger::initialize() failed to validate application log file (%s) permissions. Error (%d)",
-                    log_file_path.c_str(), has_file_permissions));
+                Log_Exception_Type::INIT,
+                Logger_Util::str_format("initialize::Init() failed to create log file directory (%s). Error (%d)",
+                                        dir_path.c_str(), error));
         }
     }
 
+    // Check file exists, otherwise attempt to make it
+    auto log_file_just_created = false;
+    if (!std::filesystem::exists(file_path)) {
+        std::ofstream out_file(file_path);
+        out_file << " == Log Start == ";
+        out_file.close();
+        log_file_just_created = true;
+    }
+
+    // Check file permissions
+    const auto has_file_permissions = Logger_Util::has_permissions_for_file(log_file_path);
+    if (!has_file_permissions) {
+        write_direct_log("Logger::initialize() failed to validate application log file (%s) permissions. Error (%d)",
+                         log_file_path.c_str(), has_file_permissions);
+        throw Logger_Exception(
+            Log_Exception_Type::PERMISSION,
+            Logger_Util::str_format("Logger::initialize() failed to validate application log file (%s) permissions. Error (%d)",
+                                    log_file_path.c_str(), has_file_permissions));
+    }
+
     s_worker.initialize(file_path);
+
+    set_log_severity_level(level);
+
+    enable_file_logging(log_to_file);
+    if (!log_file_just_created) {
+        Logger::log(Log_Level::MANUAL, " ");
+    }
+    enable_console_logging(log_to_console);
+
+    Logger::notice("yuki > Logger Initialized.");
 }
 
 void Logger::set_log_severity_level(Log_Level level) { s_worker.m_severity_level = level; }

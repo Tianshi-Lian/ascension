f3e157783584adea7b37ac614a9611a637793594
Rob Graham <r.ggraham@hotmail.com>
2023-02-26 10:36:43 +0000

    feat(github): Add ninja download for linux

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,13 +1,18 @@
 @echo off
 
-SET build_args=-j 16
+@REM We're going to run with 80% of available threads by default. As we can't use FP division,
+@REM we multiply by by then divide by 12 for the equivalent of nproc*0.8.
+FOR /F "tokens=*" %%g IN ('nproc') do (SET systhreads=%%g)
+SET /A nthreads= (%systhreads%*10)/12
+
+SET build_args=-j %nthreads%
 
 if not exist "..\build\build.ninja" (
     cmake -S ..\ -B ..\build -G"Ninja" -DCMAKE_C_COMPILER="D:/Tools/mingw64/bin/clang.exe" -DCMAKE_CXX_COMPILER="D:/Tools/mingw64/bin/clang++.exe"
 )
 
 pushd ..\build
-ninja %build_args%
+ninja -%build_args%
 popd
 
 if not exist "..\build\ascension\bin\logs" (

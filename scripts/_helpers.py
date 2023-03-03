98c5560073885a3f2e044cccf3747a1a583d39de
Rob Graham <r.ggraham@hotmail.com>
2023-03-03 21:30:21 +0000

    fix(unreleased)(scripts):Fix argument parsing

--- scripts/_helpers.py
+++ scripts/_helpers.py
@@ -1,20 +1,20 @@
 from collections import namedtuple
 
 ParsedArgs = namedtuple("ParsedArgs", "command original dict")
-Version = namedtuple("Version", "MAJOR MINOR PATCH BUILD")
 
 def process_args(argv) -> ParsedArgs:
     dict = {} # key:value dictionary
 
     for arg in argv[1:]:
         #key || key=value
-        try:
-            k = arg[0:arg.index("=")]
-            v = arg[arg.index("=") + 1:]
-        except:
-            k = arg
-            v = 0
-        dict[k] = v
+        if arg.startswith('-'):
+            try:
+                k = arg[1:arg.index("=")]
+                v = arg[arg.index("=") + 1:]
+            except:
+                k = arg
+                v = 0
+            dict[k] = v
 
     return ParsedArgs(argv[0], argv, dict)
 

251825513ddb4460d553f20c559c774ba66bdc98
Rob Graham <r.ggraham@hotmail.com>
2023-03-10 22:13:21 +0000

    chore(erika/plugins): Setup skeleton for erika plugin architecture

---
+++ erika/include/erika/core/defines.hpp
@@ -0,0 +1,16 @@
+
+// Exports
+#ifdef ERIKA_EXPORT
+#ifdef _MSC_VER
+#define ERIKA_API __declspec(dllexport)
+#else
+#define ERIKA_API __attribute__((visibility("default")))
+#endif
+#else
+// Imports
+#ifdef _MSC_VER
+#define ERIKA_API __declspec(dllimport)
+#else
+#define ERIKA_API
+#endif
+#endif

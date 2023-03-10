ec82fe26f8dbf3bdc79a294a77a76991b9a314a3
Rob Graham <r.ggraham@hotmail.com>
2023-03-10 22:13:21 +0000

    chore(erika/plugins): Setup skeleton for erika plugin architecture

---
+++ erika/plugins/renderer/d3d11/include/erika/plugins/d3d11/d3d11_main.hpp
@@ -0,0 +1,9 @@
+
+#include "erika/core/defines.hpp"
+
+namespace erika::plugins::renderer::d3d11 {
+
+ERIKA_API u32
+test_get_int();
+
+}
---
+++ erika/plugins/renderer/d3d11/src/d3d11_main.cpp
@@ -0,0 +1,11 @@
+#include "d3d11_main.hpp"
+
+namespace erika::plugins::renderer::d3d11 {
+
+u32
+test_get_int()
+{
+    return 1337;
+}
+
+}

ff8871d6b6d6495f96f2d9f4c83797d5bf43a570
Rob Graham <r.ggraham@hotmail.com>
2023-03-11 19:55:57 +0000

    docs(erika/core): add documentation to erika core

--- erika/include/erika/core/engine.hpp
+++ erika/include/erika/core/engine.hpp
@@ -4,9 +4,22 @@ namespace erika {
 
 class Game;
 
+/**
+ * @class Engine
+ *
+ * @brief Core engine class for the erika game engine.
+ */
 class Engine final {
   public:
+    /**
+     * @brief Initialize the game engine.
+     */
     static void initialize();
+    /**
+     * @brief Start running a game on top of the game engine.
+     *
+     * @param     game      a shared pointer to the game to execute
+     */
     static void run(const std::shared_ptr<Game>& game);
 };
 

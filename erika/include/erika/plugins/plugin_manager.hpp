7e286c06ea29a39bbe2273633966b461ab8b02e6
Rob Graham <r.ggraham@hotmail.com>
2023-03-11 19:51:42 +0000

    docs(erika/plugins): add documentation to the plugin system

--- erika/include/erika/plugins/plugin_manager.hpp
+++ erika/include/erika/plugins/plugin_manager.hpp
@@ -8,16 +8,43 @@ class Renderer;
 template<typename T>
 class Plugin_Factory;
 
+/**
+ * @class Plugin_Manager
+ *
+ * @brief A class for handling the plugin-based architecture of erika. Handles register, creation & lifecycle of plugins
+ */
 class Plugin_Manager {
   public:
     Plugin_Manager();
 
     // TODO: initialize() method for loading plugins. See plugins/plugin_types.hpp for more infomation.
 
+    /**
+     * @brief Register a renderer plugin with the manager.
+     *
+     * @param     name      the name of the renderer plugin
+     * @param     factory   a shared pointer to a factory which can create this renderer plugin
+     */
     void register_renderer(std::string name, const std::shared_ptr<Plugin_Factory<Renderer>>& factory);
+    /**
+     * @brief Change the active renderer plugin.
+     * This will create a new plugin and call shutdown() & initialize() if appropriate.
+     *
+     * @param   name    the name of the renderer plugin to create & make active; must be registered
+     */
     void change_active_renderer(const std::string& name);
 
+    /**
+     * @brief Get a list of the registered renderer plugins.
+     *
+     * @returns     string list containing the names of all registered renderer plugins
+     */
     [[nodiscard]] std::vector<std::string> get_registered_renderers() const;
+    /**
+     * @brief Get the current active renderer plugin.
+     *
+     * @returns     a shared pointer to the active renderer plugin, if none are active returns nullptr
+     */
     [[nodiscard]] std::shared_ptr<Renderer> get_active_renderer() const;
 
   private:

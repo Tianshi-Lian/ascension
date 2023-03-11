b204ad434411f5308b85ba05badbacc9bd69147e
Rob Graham <r.ggraham@hotmail.com>
2023-03-11 19:51:42 +0000

    docs(erika/plugins): add documentation to the plugin system

--- erika/include/erika/plugins/plugin_types.hpp
+++ erika/include/erika/plugins/plugin_types.hpp
@@ -2,12 +2,22 @@
 
 namespace erika::plugins {
 
+/**
+ * @enum Plugin_Type
+ *
+ * @brief Specifies the types of plugins available
+ */
 enum class Plugin_Type : u32 {
     Unknown = 0,
-    Renderer = 1,
-    Count
+    Renderer = 1
 };
 
+/**
+ * @class Plugin
+ *
+ * @brief A base class for any erika engine plugin. All plugins which should be handled automatically by the Plugin_Manager
+ * should derive from this base.
+ */
 class Plugin {
   public:
     // TODO: replace passing in an explicit type with using sub dirs in the game distribution folder
@@ -19,10 +29,26 @@ class Plugin {
     explicit Plugin(Plugin_Type type, std::string name);
     virtual ~Plugin() = default;
 
+    /**
+     * @brief Initialize this plugin.
+     */
     virtual void initialize() = 0;
+    /**
+     * @brief Shutdown this plugin.
+     */
     virtual void shutdown() = 0;
 
+    /**
+     * @brief Get the name of this plugin.
+     *
+     * @returns     string representing the plugin name
+     */
     [[nodiscard]] const std::string& get_name() const;
+    /**
+     * @brief Get the type of this plugin.
+     *
+     * @returns     Plugin_Type specifying the type of this plugin
+     */
     [[nodiscard]] const Plugin_Type& get_type() const;
 
     Plugin(const Plugin&) = default;
@@ -35,12 +61,23 @@ class Plugin {
     std::string m_name;
 };
 
+/**
+ * @class Plugin_Factory
+ *
+ * @brief A base class for any erika plugin factories. All plugins which should be handled automatically by the Plugin_Manager
+ * must have a respective Plugin_Factory for their type.
+ */
 template<typename T>
 class Plugin_Factory {
   public:
     Plugin_Factory<T>() = default;
     virtual ~Plugin_Factory<T>() = default;
 
+    /**
+     * @brief Create a new plugin.
+     *
+     * @returns     a shared pointer containing a new plugin of the type handled by this factory
+     */
     virtual std::shared_ptr<T> create() = 0;
 
     Plugin_Factory(const Plugin_Factory&) = delete;
--- erika/include/erika/plugins/renderer.hpp
+++ erika/include/erika/plugins/renderer.hpp
@@ -4,6 +4,12 @@
 
 namespace erika::plugins {
 
+/**
+ * @class Renderer
+ *
+ * @brief A base class for all renderer plugins. Any plugin which provides rendering capabilities, such as DirectX11, Vulkan,
+ * OpenGL etc must inherit from this base class.
+ */
 class Renderer : public Plugin {
 
   public:
@@ -14,7 +20,13 @@ class Renderer : public Plugin {
 
     ~Renderer() override = default;
 
+    /**
+     * @brief Begin drawing a new scene.
+     */
     virtual void begin_scene() = 0;
+    /**
+     * @brief Finish drawing the current scene.
+     */
     virtual void end_scene() = 0;
 
     Renderer(const Renderer&) = default;
@@ -23,6 +35,12 @@ class Renderer : public Plugin {
     Renderer& operator=(Renderer&&) = delete;
 };
 
+/**
+ * @class Renderer_Plugin_Factory
+ *
+ * @brief A base class for any erika renderer plugin factories. Any plugin which provides rendering capabilities, such as
+ * DirectX11, Vulkan, OpenGL etc must have a respective Renderer_Plugin_Factory for their type.
+ */
 using Renderer_Plugin_Factory = erika::plugins::Plugin_Factory<Renderer>;
 
 }

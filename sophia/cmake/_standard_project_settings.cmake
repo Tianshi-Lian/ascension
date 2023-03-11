f9b0d22dc40b151bcf8c7c052fbb31f66484b80b
Rob Graham <r.ggraham@hotmail.com>
2023-03-11 15:23:29 +0000

    feat(sophia/helpers): implement support for multi-config generators

--- sophia/cmake/_standard_project_settings.cmake
+++ sophia/cmake/_standard_project_settings.cmake
@@ -2,42 +2,63 @@ include_guard()
 
 # Set common project settings for this CMake project
 function(set_standard_project_settings)
-    # Set a default built type if none specified
-    if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
-        message(
-            STATUS "Setting build type to 'Debug' as none were specified."
-        )
-        set(CMAKE_BUILD_TYPE
-            Debug
-            CACHE STRING "Choose the type of build." FORCE)
-        set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
-    endif()
+	# Setup the build configurations for single or multi config generators
+	if(NOT SET_UP_CONFIGURATIONS_DONE)
+		set(SET_UP_CONFIGURATIONS_DONE TRUE)
 
-    # Finds and activates ccache for builds
-    find_program(CCACHE ccache)
+		get_property(isMultiConfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
 
-    if(CCACHE)
-        message(STATUS "Using ccache.")
-        set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE})
-        set(CMAKE_CACHEFILE_DIR ${CMAKE_BINARY_DIR})
-    else()
-        message(STATUS "Could not find ccache.")
-    endif()
+		if(isMultiConfig)
+			message(STATUS "Using multi-config")
+			set(CMAKE_CONFIGURATION_TYPES "Debug;Release;RelWithDebInfo" CACHE STRING "" FORCE)
+		else()
+			if(NOT CMAKE_BUILD_TYPE)
+				message("Defaulting to debug build")
+				set(CMAKE_BUILD_TYPE Debug CACHE STRING "" FORCE)
+			endif()
 
-    # Generates compile_commands.json
-    set(CMAKE_EXPORT_COMPILE_COMMANDS ON CACHE INTERNAL "")
+			set_property(CACHE CMAKE_BUILD_TYPE PROPERTY HELPSTRING "Choose the type of build")
+			set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug;Release;RelWithDebInfo")
+		endif()
 
-    # Disable compiler extensions to ensure compliance with ISO C++ standards
-    set(CMAKE_CXX_STANDARD_REQUIRED ON)
-    set(CMAKE_CXX_EXTENSIONS OFF)
+		set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -g -O0")
+		set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -g -O0")
+		set(CMAKE_EXE_LINKER_FLAGS_DEBUG "${CMAKE_EXE_LINKER_FLAGS_DEBUG}")
 
-    # Enable link time optimisation by default for release
-    if(${CMAKE_BUILD_TYPE} STREQUAL "Release")
-        include(CheckIPOSupported)
-        check_ipo_supported(RESULT result)
+		set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O3 -DNDEBUG")
+		set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3 -DNDEBUG")
+		set(CMAKE_EXE_LINKER_FLAGS_RELEASE "${CMAKE_EXE_LINKER_FLAGS_RELEASE}")
 
-        if(result)
-            set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ON)
-        endif()
-    endif()
+		set(CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS_RELWITHDEBINFO} -O2 -g")
+		set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} -O2 -g")
+		set(CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO "${CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO}")
+
+		# Enable link time optimization by default for release builds
+		include(CheckIPOSupported)
+		check_ipo_supported(RESULT result)
+
+		if(result)
+			message(STATUS "Using LTO for release builds")
+			set(CMAKE_INTERPROCEDURAL_OPTIMIZATION_RELEASE ON)
+			set(CMAKE_INTERPROCEDURAL_OPTIMIZATION_RELWITHDEBINFO ON)
+		endif()
+	endif()
+
+	# Finds and activates ccache for builds
+	find_program(CCACHE ccache)
+
+	if(CCACHE)
+		message(STATUS "Using ccache")
+		set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE})
+		set(CMAKE_CACHEFILE_DIR ${CMAKE_BINARY_DIR})
+	else()
+		message(STATUS "Could not find ccache")
+	endif()
+
+	# Generates compile_commands.json
+	set(CMAKE_EXPORT_COMPILE_COMMANDS ON CACHE INTERNAL "")
+
+	# Disable compiler extensions to ensure compliance with ISO C++ standards
+	set(CMAKE_CXX_STANDARD_REQUIRED ON)
+	set(CMAKE_CXX_EXTENSIONS OFF)
 endfunction()

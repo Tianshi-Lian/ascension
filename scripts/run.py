999d98353cf29a0ecb4bb50970fb7a216b29bea1
Rob Graham <r.ggraham@hotmail.com>
2023-03-11 15:24:30 +0000

    feat(repo/scripts): implement config script to run cmake & support multi-config

--- scripts/run.py
+++ scripts/run.py
@@ -1,11 +1,15 @@
-import _globals
+import _globals, _helpers
 import subprocess
 import sys
 from collections import namedtuple
 
 def run():
+    args = _helpers.process_args(sys.argv)
+
+    build_type = _helpers.get_arg_value(args, ["build-type", "bt"], _globals.BUILD_TYPE)
+
     exit_code = _globals.SUCCESS
-    workingDir = f'build/{_globals.BUILD_PROJECT_NAME}/bin'
+    workingDir = f'build/{_globals.BUILD_PROJECT_NAME}/bin/{build_type}'
     if _globals.PLATFORM == "windows":
         process = subprocess.run([f'{workingDir}/{_globals.BUILD_PROJECT_NAME}'], cwd=workingDir)
         exit_code = process.returncode

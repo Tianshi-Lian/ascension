da8fd34c2838305a6e4c465d8928a11e6a6adac5
Rob Graham <r.ggraham@hotmail.com>
2023-03-12 12:20:33 +0000

    chore(repo/scripts): add optional compiler settings to .env file

--- scripts/_globals.py
+++ scripts/_globals.py
@@ -3,15 +3,23 @@ import sys
 
 from dotenv import load_dotenv
 
-
-load_dotenv()
-
-BUILD_TYPE = os.getenv('BUILD_TYPE')
-BUILD_PROJECT_NAME = os.getenv('PROJECT_NAME')
-
 SUCCESS = 0
 
 PLATFORM = \
     'linux' if 'linux' in sys.platform else \
     'windows' if 'win' in sys.platform else \
     'unknown'
+
+load_dotenv()
+
+BUILD_TYPE = os.getenv(
+    'BUILD_TYPE') if 'BUILD_TYPE' in os.environ else 'Debug'
+
+BUILD_PROJECT_NAME = os.getenv(
+    'PROJECT_NAME') if 'PROJECT_NAME' in os.environ else 'unknown'
+
+C_COMPILER = os.getenv(
+    'C_COMPILER') if 'BUILD_TYPE' in os.environ else 'clang'
+
+CXX_COMPILER = os.getenv(
+    'CXX_COMPILER') if 'CXX_COMPILER' in os.environ else 'clang++'

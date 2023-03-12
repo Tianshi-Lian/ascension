38d1886b0ed3cfcae2dcce390d81a2a7e80c419a
Rob Graham <r.ggraham@hotmail.com>
2023-03-12 09:28:09 +0000

    chore(erika/core): platform layer usage example

--- erika/include/erika/core/entry.hpp
+++ erika/include/erika/core/entry.hpp
@@ -6,13 +6,15 @@
 #include "yuki/debug/instrumentor.hpp"
 #include "yuki/debug/logger.hpp"
 
-#include <iostream>
+#include "yuki/platform/platform.hpp"
 
 int
-main() // NOLINT
+main(int /*argc*/, char** /*args*/) // NOLINT
 {
     yuki::debug::Logger::initialize("logs/app.log", yuki::debug::Log_Level::DEBUG, true, true);
     PROFILE_BEGIN_SESSION("erika_profiling", "logs/timings.json");
+
+    yuki::platform::Platform::initialize(std::make_shared<int>(1), "", 0, 0, 0, 0);
 
     erika::Engine engine;
     engine.initialize();

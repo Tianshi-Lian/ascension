635f074759941220a99daf4d080b3080a0e800ba
Rob Graham <r.ggraham@hotmail.com>
2023-03-12 09:27:51 +0000

    chore(yuki/platform): initial setup for win32 platform layer

---
+++ yuki/src/platform/sources.cmake
@@ -0,0 +1,12 @@
+include_guard()
+
+set(LIB_SRC_FILES
+	${LIB_SRC_FILES}
+	src/platform/platform.cpp
+	src/platform/platform_win32.cpp
+)
+
+set(LIB_INC_FILES
+	${LIB_INC_FILES}
+	include/${LIB_NAME}/platform/platform.hpp
+)

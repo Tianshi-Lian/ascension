20ad70356a7770f0be8437e89b3bb4507e3178e3
Rob Graham <r.ggraham@hotmail.com>
2023-03-19 17:15:34 +0000

    refactor(erika/plugins): refactor d3d11 plugin to use platform loading

--- erika/plugins/renderer/d3d11/include/erika/plugins/renderer/d3d11/d3d11.hpp
+++ erika/plugins/renderer/d3d11/include/erika/plugins/renderer/d3d11/d3d11.hpp
@@ -1,11 +1,10 @@
 #pragma once
 
-#include "erika/core/defines.hpp"
+#include "erika/plugins/plugin_manager.hpp"
 #include "erika/plugins/renderer.hpp"
 
 namespace erika::plugins::renderer::d3d11 {
-
-class ERIKA_API D3D11_Renderer : public erika::plugins::Renderer {
+class D3D11_Renderer : public erika::plugins::Renderer {
   public:
     D3D11_Renderer();
     ~D3D11_Renderer() override = default;
@@ -22,17 +21,7 @@ class ERIKA_API D3D11_Renderer : public erika::plugins::Renderer {
     D3D11_Renderer& operator=(D3D11_Renderer&&) = delete;
 };
 
-class ERIKA_API D3D11_Renderer_Factory : public Renderer_Plugin_Factory {
-  public:
-    D3D11_Renderer_Factory() = default;
-    ~D3D11_Renderer_Factory() override = default;
-
-    std::shared_ptr<Renderer> create() override;
-
-    D3D11_Renderer_Factory(const D3D11_Renderer_Factory&) = delete;
-    D3D11_Renderer_Factory& operator=(const D3D11_Renderer_Factory&) = delete;
-    D3D11_Renderer_Factory(D3D11_Renderer_Factory&&) = delete;
-    D3D11_Renderer_Factory& operator=(D3D11_Renderer_Factory&&) = delete;
-};
-
 }
+
+extern "C" void
+registerPlugin(erika::plugins::Plugin_Manager& plugin_manager);
--- erika/plugins/renderer/d3d11/src/d3d11.cpp
+++ erika/plugins/renderer/d3d11/src/d3d11.cpp
@@ -4,6 +4,19 @@
 
 namespace erika::plugins::renderer::d3d11 {
 
+class D3D11_Renderer_Factory : public Renderer_Plugin_Factory {
+  public:
+    D3D11_Renderer_Factory() = default;
+    ~D3D11_Renderer_Factory() override = default;
+
+    std::shared_ptr<Renderer> create() override;
+
+    D3D11_Renderer_Factory(const D3D11_Renderer_Factory&) = delete;
+    D3D11_Renderer_Factory& operator=(const D3D11_Renderer_Factory&) = delete;
+    D3D11_Renderer_Factory(D3D11_Renderer_Factory&&) = delete;
+    D3D11_Renderer_Factory& operator=(D3D11_Renderer_Factory&&) = delete;
+};
+
 D3D11_Renderer::D3D11_Renderer()
   : erika::plugins::Renderer("D3D11_Renderer")
 {
@@ -36,9 +49,19 @@ D3D11_Renderer::end_scene()
 std::shared_ptr<Renderer>
 D3D11_Renderer_Factory::create()
 {
-    yuki::debug::Logger::initialize("logs/renderer.log", yuki::debug::Log_Level::DEBUG, true, true);
     yuki::debug::Logger::debug("erika > D3D11_Renderer_Factory::create()");
     return std::make_shared<D3D11_Renderer>();
 }
 
+}
+
+void
+registerPlugin(erika::plugins::Plugin_Manager& plugin_manager)
+{
+    yuki::debug::Logger::initialize("logs/renderer.log", yuki::debug::Log_Level::DEBUG, true, true);
+    yuki::debug::Logger::debug("erika > D3D11_Renderer::registerPlugin()");
+
+    plugin_manager.register_renderer(
+        "D3D11_Renderer", std::make_shared<erika::plugins::renderer::d3d11::D3D11_Renderer_Factory>()
+    );
 }

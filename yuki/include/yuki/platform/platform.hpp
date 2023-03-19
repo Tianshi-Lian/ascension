5cdd160aa0a8ee0fae0a70cd36117e48682ff116
Rob Graham <r.ggraham@hotmail.com>
2023-03-19 20:51:02 +0000

    docs(yuki/platform): add class & function documentation to platform.hpp

--- yuki/include/yuki/platform/platform.hpp
+++ yuki/include/yuki/platform/platform.hpp
@@ -4,19 +4,45 @@
 
 namespace yuki::platform {
 
-class Platform_State {
-  public:
+/**
+ * @struct Platform_State
+ *
+ * @brief A struct holding platform-independent state.
+ */
+struct Platform_State {
     std::shared_ptr<void> internal_state;
 };
 
-class Library_Handle {
-  public:
+/**
+ * @struct Library_Handle
+ *
+ * @brief A struct holding a platform-independent handle to a shared library.
+ */
+struct Library_Handle {
     std::string filepath;
     std::shared_ptr<void> internal_state;
 };
 
+/**
+ * @class Platform
+ *
+ * @brief A class which describes a platform layer for the applications. Each supported platform will
+ * need to implement the functions in this class in a corresponding .cpp file.
+ */
 class Platform {
   public:
+    /**
+     * @brief Initialize the platform layer.
+     *
+     * @param       platform_state      a pointer to the current platform state
+     * @param       application_name    name of the application being ran
+     * @param       pos_x               starting x position of the application window
+     * @param       pos_y               starting x position of the application window
+     * @param       width               starting width of the application window
+     * @param       height              starting height of the application window
+     *
+     * @return      true if initialization was successful, else false
+     */
     static bool initialize(
         const std::shared_ptr<Platform_State>& platform_state,
         const std::string& application_name,
@@ -26,13 +52,53 @@ class Platform {
         u32 height
     );
 
+    /**
+     * @brief Shutdown the application layer.
+     *
+     * @param       platform_state      a pointer to the current platform state
+     */
     static void shutdown(const std::shared_ptr<Platform_State>& platform_state);
+    /**
+     * @brief Process any platform-specific messages.
+     *
+     * @param       platform_state      a pointer to the current platform state
+     *
+     * @return      true if the application should continue running, else false
+     */
     static bool process_messages(const std::shared_ptr<Platform_State>& platform_state);
+
+    /**
+     * @brief Sleep the active thread for the specific time.
+     *
+     * @param       milliseconds      then number of milliseconds to sleep for
+     */
     static void sleep(u32 milliseconds);
 
+    /**
+     * @brief Load a shared library from disk.
+     *
+     * @param     filepath      filepath to the library to load
+     *
+     * @return    handle to the loaded library, if successful then internal_state
+     * will not be a nullptr
+     *
+     */
     static Library_Handle load_shared_library(const std::string& filepath);
+    /**
+     * @brief Free a loaded shared library.
+     *
+     * @param     library_handle      reference to a loaded library handle to free
+     */
     static void free_shared_library(const Library_Handle& library_handle);
 
+    /**
+     * @brief Load an exported function from a loaded library.
+     *
+     * @param     library_handle      reference to a loaded library to load from
+     * @param     function_name       name of the function to attempt to load
+     *
+     * @return    function<T> holding the loaded function, else nullptr
+     */
     template<typename T>
     static std::function<T> load_library_function(const Library_Handle& library_handle, const std::string& function_name)
     {

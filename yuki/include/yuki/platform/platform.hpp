b2b097fe0f8b3a54b7ca55ac00815db379997a75
Rob Graham <r.ggraham@hotmail.com>
2023-03-20 19:47:58 +0000

    refactor(yuki/repo): move platform types out of platform; add missing include guards

--- yuki/include/yuki/platform/platform.hpp
+++ yuki/include/yuki/platform/platform.hpp
@@ -1,27 +1,11 @@
+#pragma once
 
 #include <functional>
 #include <memory>
 
+#include "platform_types.hpp"
+
 namespace yuki::platform {
-
-/**
- * @struct Platform_State
- *
- * @brief A struct holding platform-independent state.
- */
-struct Platform_State {
-    std::shared_ptr<void> internal_state;
-};
-
-/**
- * @struct Library_Handle
- *
- * @brief A struct holding a platform-independent handle to a shared library.
- */
-struct Library_Handle {
-    std::string filepath;
-    std::shared_ptr<void> internal_state;
-};
 
 /**
  * @class Platform

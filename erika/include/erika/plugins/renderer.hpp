026260c98ab32a67d5e05e7a7e9935ae5bcc2683
Rob Graham <r.ggraham@hotmail.com>
2023-03-25 11:41:21 +0000

    docs(erika/plugins)[renderer]: add and update renderer documentation

--- erika/include/erika/plugins/renderer.hpp
+++ erika/include/erika/plugins/renderer.hpp
@@ -16,25 +16,43 @@ namespace erika::plugins {
  */
 class Renderer : public Plugin {
   public:
-    struct Clear_Colour {
+    /**
+     * @struct Clear_Color
+     *
+     * @brief A struct for storing the RGBA colour used for clearing the back buffer.
+     */
+    struct Clear_Color {
         f32 r;
         f32 g;
         f32 b;
         f32 a;
     };
-    static constexpr Clear_Colour Default_Clear_Colour = { 0.79f, 0.94f, 0.7f, 1.0f };
+    static constexpr Clear_Color Default_Clear_Color = { 0.79f, 0.94f, 0.7f, 1.0f };
 
     explicit Renderer();
     ~Renderer() override = default;
 
+    /**
+     * @brief Set the screen size the renderer should draw to.
+     *
+     * @param   width     the width of the screen
+     * @param   height    the height of the screen
+     */
     void set_screen_size(const u32 width, const u32 height);
 
+    /**
+     * @brief Initialize the renderer plugin.
+     *
+     * @param     platform_state      a shared pointer to the OS platform_state
+     */
     virtual void initialize(const std::shared_ptr<yuki::platform::Platform_State>& platform_state) = 0;
 
     /**
      * @brief Begin drawing a new scene.
+     *
+     * @param     clear_color     an RGBA color to clear the back buffer with before rendering starts
      */
-    virtual void begin_scene(const Clear_Colour& clear_color = Default_Clear_Colour) = 0;
+    virtual void begin_scene(const Clear_Color& clear_color = Default_Clear_Color) = 0;
     /**
      * @brief Finish drawing the current scene.
      */
@@ -46,9 +64,26 @@ class Renderer : public Plugin {
     Renderer& operator=(Renderer&&) = delete;
 
   protected:
+    /**
+     * @brief Get the width of the screen.
+     *
+     * @return    u32     the current screen width
+     */
     [[nodiscard]] u32 get_screen_width() const;
+    /**
+     * @brief Get the height of the screen.
+     *
+     * @return    u32     the current screen height
+     */
     [[nodiscard]] u32 get_screen_height() const;
 
+    /**
+     * @brief Callback for when the renderer is due to be resized.
+     * m_screen_width and m_screen_height will be updated after this call and therefore can be used to check the previous size.
+     *
+     * @param     width     the new width of the screen
+     * @param     height    the new height of the screen
+     */
     virtual void on_resize(const u32 width, const u32 height) = 0;
 
   private:

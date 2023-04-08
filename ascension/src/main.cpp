5faff5230d87472edec868f82cda6847c9e7800e
Rob Graham <r.ggraham@hotmail.com>
2023-04-08 16:28:39 +0100

    feat(ascension)[core]: get application & window running

--- ascension/src/main.cpp
+++ ascension/src/main.cpp
@@ -1,17 +1,23 @@
-#include <iostream>
+#include "core/application.hpp"
 
 #include <SDL.h>
-#include <stb/stb_image.h>
 
 #define UNUSED_PARAM(a) (void)a
+
+constexpr glm::vec2 Default_Window_Position{ 200, 200 };
+constexpr glm::vec2 Default_Window_Size{ 1600, 900 };
 
 int
 main(int argc, char** argv)
 {
+    using namespace ascension;
+
     UNUSED_PARAM(argc);
     UNUSED_PARAM(argv);
 
-    std::cout << "Hello, world!" << std::endl;
+    core::Application app;
+    app.setup("Ascension", Default_Window_Position, Default_Window_Size);
+    app.run();
 
     return 0;
 }

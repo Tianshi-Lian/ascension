356c59521f11d55f737f0cb24a1c811040be56aa
Rob Graham <r.ggraham@hotmail.com>
2023-04-09 16:13:00 +0100

    chore(ascension)[repo]: license ascension application

--- ascension/src/main.cpp
+++ ascension/src/main.cpp
@@ -1,3 +1,27 @@
+/**
+ * File: main.cpp
+ * Project: ascension
+ * File Created: 2023-04-06 21:17:10
+ * Author: Rob Graham (robgrahamdev@gmail.com)
+ * Last Modified: 2023-04-09 16:03:20
+ * ------------------
+ * Copyright 2023 Rob Graham
+ * ==================
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * ==================
+ */
+
 #include "core/application.hpp"
 
 #include <SDL.h>

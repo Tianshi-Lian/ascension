1439cec231beca3033967d74ecd07f8c3cb2329d
Rob Graham <r.ggraham@hotmail.com>
2023-04-09 16:19:41 +0100

    chore(sophia)[repo]: license sophia scripts

--- sophia/clean.py
+++ sophia/clean.py
@@ -1,3 +1,27 @@
+'''
+  ' File: clean.py
+  ' Project: sophia
+  ' File Created: 2023-03-03 20:33:35
+  ' Author: Rob Graham (robgrahamdev@gmail.com)
+  ' Last Modified: 2023-04-09 16:18:16
+  ' ------------------
+  ' Copyright 2023 Rob Graham
+  ' ==================
+  ' This program is free software: you can redistribute it and/or modify
+  ' it under the terms of the GNU Affero General Public License as published
+  ' by the Free Software Foundation, either version 3 of the License, or
+  ' (at your option) any later version.
+  '
+  ' This program is distributed in the hope that it will be useful,
+  ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+  ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  ' GNU Affero General Public License for more details.
+  '
+  ' You should have received a copy of the GNU Affero General Public License
+  ' along with this program.  If not, see <https://www.gnu.org/licenses/>.
+  ' ==================
+'''
+
 import os
 import shutil
 import sys

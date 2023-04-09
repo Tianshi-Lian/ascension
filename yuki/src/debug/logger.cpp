bc88579911f5773d437d1530055aa57fbed5c118
Rob Graham <r.ggraham@hotmail.com>
2023-04-09 16:11:19 +0100

    chore(yuki)[repo]: license yuki library

--- yuki/src/debug/logger.cpp
+++ yuki/src/debug/logger.cpp
@@ -1,3 +1,27 @@
+/**
+ * File: logger.cpp
+ * Project: yuki
+ * File Created: 2023-02-25 11:46:28
+ * Author: Rob Graham (robgrahamdev@gmail.com)
+ * Last Modified: 2023-04-09 16:07:06
+ * ------------------
+ * Copyright 2023 Rob Graham
+ * ==================
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * ==================
+ */
+
 #include <filesystem>
 #include <iostream>
 

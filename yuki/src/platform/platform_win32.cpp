f6b4a7d6115b1c44b6982a278bc061fa562e8f6c
Rob Graham <r.ggraham@hotmail.com>
2023-04-09 16:11:19 +0100

    chore(yuki)[repo]: license yuki library

--- yuki/src/platform/platform_win32.cpp
+++ yuki/src/platform/platform_win32.cpp
@@ -1,3 +1,27 @@
+/**
+ * File: platform_win32.cpp
+ * Project: yuki
+ * File Created: 2023-03-12 21:20:07
+ * Author: Rob Graham (robgrahamdev@gmail.com)
+ * Last Modified: 2023-04-09 16:07:16
+ * ------------------
+ * Copyright 2023 Rob Graham
+ * ==================
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * ==================
+ */
+
 #include "platform/platform.hpp"
 
 #ifdef _WIN32

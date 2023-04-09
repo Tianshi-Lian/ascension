57fe904bfc20d0d9bb581807bde7cbe2f4e1a13b
Rob Graham <r.ggraham@hotmail.com>
2023-04-09 16:19:41 +0100

    chore(sophia)[repo]: license sophia scripts

--- sophia/_cli.py
+++ sophia/_cli.py
@@ -1,9 +1,34 @@
+'''
+  ' File: _cli.py
+  ' Project: sophia
+  ' File Created: 2023-03-03 19:36:23
+  ' Author: Rob Graham (robgrahamdev@gmail.com)
+  ' Last Modified: 2023-04-09 16:16:40
+  ' ------------------
+  ' Copyright 2023 Rob Graham
+  ' ==================
+  ' This program is free software: you can redistribute it and/or modify
+  ' it under the terms of the GNU Affero General Public License as published
+  ' by the Free Software Foundation, either version 3 of the License, or
+  ' (at your option) any later version.
+  '
+  ' This program is distributed in the hope that it will be useful,
+  ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+  ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  ' GNU Affero General Public License for more details.
+  '
+  ' You should have received a copy of the GNU Affero General Public License
+  ' along with this program.  If not, see <https://www.gnu.org/licenses/>.
+  ' ==================
+'''
+
 import importlib
 import os
 import subprocess
 import sys
 
 SCRIPTS_DIR = "sophia"
+
 
 def import_or_install(package):
     try:
@@ -14,6 +39,8 @@ def import_or_install(package):
 
 # TODO: setup some awareness of "command chains/dependencies" where we can register command dependencies and ensure
 # /t    all of the dependencies run before trying to execute that command. E.g. configure -> build -> install -> run.
+
+
 def run_command(commands):
     exit_code = 0
 

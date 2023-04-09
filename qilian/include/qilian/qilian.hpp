423aa33ca04c330d4274c72d7ccbc24f4c8e7cba
Rob Graham <r.ggraham@hotmail.com>
2023-04-09 16:12:39 +0100

    chore(qilian)[repo]: license qilian application

--- qilian/include/qilian/qilian.hpp
+++ qilian/include/qilian/qilian.hpp
@@ -1,3 +1,27 @@
+/**
+ * File: qilian.hpp
+ * Project: qilian
+ * File Created: 2023-04-04 18:39:29
+ * Author: Rob Graham (robgrahamdev@gmail.com)
+ * Last Modified: 2023-04-09 16:06:46
+ * ------------------
+ * Copyright 2023 Rob Graham
+ * ==================
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * ==================
+ */
+
 #pragma once
 
 #include "erika/core/game.hpp"

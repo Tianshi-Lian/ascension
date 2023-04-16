e7ca80648c49138ae4233dee8d216cb885664b51
Rob Graham <r.ggraham@hotmail.com>
2023-04-16 15:28:00 +0100

    chore(ascension)[graphics]: wip sprite_batch rendering

--- ascension/src/graphics/sprite_batch.cpp
+++ ascension/src/graphics/sprite_batch.cpp
@@ -3,7 +3,7 @@
  * Project: ascension
  * File Created: 2023-04-15 14:54:44
  * Author: Rob Graham (robgrahamdev@gmail.com)
- * Last Modified: 2023-04-15 15:43:50
+ * Last Modified: 2023-04-16 15:23:20
  * ------------------
  * Copyright 2023 Rob Graham
  * ==================
@@ -24,46 +24,55 @@
 
 #include "graphics/sprite_batch.hpp"
 
-#include <cstddef>
+#include <GL/glew.h>
+#include <limits>
+
+#include <glm/ext/matrix_clip_space.hpp>
 
 #include "graphics/buffer_object.hpp"
 #include "graphics/shader.hpp"
-// #include "graphics/texture_2d.hpp"
+#include "graphics/texture_2d.hpp"
 #include "graphics/vertex_array_object.hpp"
 
 #include "yuki/debug/logger.hpp"
 
 namespace ascension::graphics {
 
-struct Sprite_Batch_Vertex {
-    v2f position;
-    v2f tex_coords;
-    v4f color;
+enum class Quad_Vertex_Index : u32 {
+    BOTTOM_LEFT = 0,
+    TOP_RIGHT,
+    TOP_LEFT,
+    BOTTOM_RIGHT
 };
 
 static constexpr u32 QUAD_VERTEX_COUNT = 4;
 static constexpr u32 QUAD_INDEX_COUNT = 6;
-// static constexpr f32 TEMP_VERTEX_BUFFER[] = {
-//     0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-// };
+static constexpr std::array<f32, 8> TEMP_VERTEX_BUFFER{
+    0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+};
 
 Sprite_Batch::Sprite_Batch()
-  : m_screen_width(0)
-  , m_screen_height(0)
-  , m_max_batch_size(0)
+  : m_max_batch_size(0)
+  , m_projection{ 1.0f }
+  , m_batch_active(false)
+  , m_active_transform{ 1.0f }
 {
 }
 
+Sprite_Batch::~Sprite_Batch()
+{
+    clear();
+}
+
 void
-Sprite_Batch::initialize(u32 screen_width, u32 screen_height, u32 max_batch_size)
+Sprite_Batch::initialize(u32 screen_width, u32 screen_height, const std::shared_ptr<Shader>& sprite_shader, u32 max_batch_size)
 {
     if (m_max_batch_size > 0) {
         yuki::debug::Logger::error("ascension", "Sprite_Batch should only be initialized once!");
         return;
     }
 
-    m_screen_width = screen_width;
-    m_screen_height = screen_height;
+    m_sprite_shader = sprite_shader;
     m_max_batch_size = max_batch_size;
 
     m_sprite_shader->bind();
@@ -102,6 +111,167 @@ Sprite_Batch::initialize(u32 screen_width, u32 screen_height, u32 max_batch_size
     m_vertex_array->set_index_buffer(m_index_buffer);
     m_vertex_array->set_attrib_ptr_list(vertex_map);
     m_vertex_array->unbind();
+
+    m_current_batch.reserve(m_max_batch_size + 1);
+
+    on_resize(screen_width, screen_height);
+}
+
+void
+Sprite_Batch::begin(m4 transform)
+{
+    if (m_batch_active) {
+        yuki::debug::Logger::warn(
+            "ascension", "Sprite_Batch::begin() has already been called. Call Sprite_Batch::end() first."
+        );
+        return;
+    }
+
+    m_batch_active = true;
+    m_active_transform = transform;
+}
+
+void
+Sprite_Batch::end()
+{
+    if (!m_batch_active) {
+        yuki::debug::Logger::warn("ascension", "Sprite_Batch::end() has already been called without an active batch.");
+        return;
+    }
+
+    std::sort(
+        m_current_batch.begin(),
+        m_current_batch.end(),
+        [](const Sprite_Batch_Item& item_a, const Sprite_Batch_Item& item_b) { return *item_a.texture > *item_b.texture; }
+    );
+
+    m_sprite_shader->bind();
+    m_sprite_shader->set_mat4f("m_projectin_view", m_projection * m_active_transform);
+
+    std::shared_ptr<Texture_2D> current_texture = m_current_batch.front().texture;
+
+    for (const auto& batch_item : m_current_batch) {
+        if (batch_item.texture != current_texture) {
+            flush(current_texture);
+            current_texture = batch_item.texture;
+        }
+
+        generate_quad_vertices(batch_item);
+
+        if ((m_current_vertices.size() / QUAD_VERTEX_COUNT) >= m_max_batch_size) {
+            flush(current_texture);
+        }
+    }
+
+    flush(current_texture);
+    clear();
+    m_batch_active = false;
+}
+
+void
+Sprite_Batch::clear()
+{
+    m_current_batch.clear();
+    m_current_vertices.clear();
+}
+
+void
+Sprite_Batch::on_resize(u32 screen_width, u32 screen_height)
+{
+#ifdef AS_DEBUG
+    static const auto float_max = static_cast<u32>(std::numeric_limits<f32>::infinity());
+    assert(float_max >= screen_width);
+    assert(float_max >= screen_height);
+#endif
+    m_projection = glm::ortho(0.0f, static_cast<f32>(screen_width), 0.0f, static_cast<f32>(screen_height), -1.0f, 1.0f);
+}
+
+void
+Sprite_Batch::draw(const std::shared_ptr<Texture_2D>& texture, v2f position)
+{
+    if (!m_batch_active) {
+        yuki::debug::Logger::warn("ascension", "Sprite_Batch::draw() has already been called without an active batch.");
+        return;
+    }
+
+    m_current_batch.emplace_back(texture, position);
+}
+
+void
+Sprite_Batch::generate_quad_vertices(const Sprite_Batch_Item& item)
+{
+    const auto texture = item.texture;
+    const auto source = v4f{ 0, 0, texture->width(), texture->height() };
+    const auto position = item.position;
+
+    const auto texel_width = 1.0f / static_cast<f32>(texture->width());
+    const auto texel_height = 1.0f / static_cast<f32>(texture->height());
+
+    glm::vec2 scale;
+    scale.x = source.z;
+    scale.y = source.w;
+
+    glActiveTexture(GL_TEXTURE0);
+
+    for (u32 i = 0; i < QUAD_VERTEX_COUNT; i++) {
+        glm::vec2 vertPosition;
+        const auto start_index = i * 2;
+
+        const auto x_pos = TEMP_VERTEX_BUFFER[start_index];
+        const auto y_pos = TEMP_VERTEX_BUFFER[start_index + 1];
+
+        const auto width = (scale.x * x_pos);
+        const auto height = (scale.y * y_pos);
+
+        glm::vec2 tex_coords;
+
+        if (i == static_cast<u32>(Quad_Vertex_Index::TOP_LEFT)) {
+            vertPosition.x = position.x;
+            vertPosition.y = position.y;
+            tex_coords.x = source.x * texel_width;
+            tex_coords.y = source.x * texel_height;
+        }
+        else if (i == static_cast<u32>(Quad_Vertex_Index::TOP_RIGHT)) {
+            vertPosition.x = position.x + width;
+            vertPosition.y = position.y;
+            tex_coords.x = (source.x + source.z) * texel_width;
+            tex_coords.y = source.y * texel_height;
+        }
+        else if (i == static_cast<u32>(Quad_Vertex_Index::BOTTOM_LEFT)) {
+            vertPosition.x = position.x;
+            vertPosition.y = position.y + height;
+            tex_coords.x = source.x * texel_width;
+            tex_coords.y = (source.y + source.w) * texel_height;
+        }
+        else if (i == static_cast<u32>(Quad_Vertex_Index::BOTTOM_RIGHT)) {
+
+            vertPosition.x = position.x + width;
+            vertPosition.y = position.y + height;
+            tex_coords.x = (source.x + source.z) * texel_width;
+            tex_coords.y = (source.y + source.w) * texel_height;
+        }
+
+        m_current_vertices.emplace_back(vertPosition, tex_coords, v4f{ 1.0f, 1.0f, 1.0f, 1.0f });
+    }
+}
+
+void
+Sprite_Batch::flush(const std::shared_ptr<Texture_2D>& texture)
+{
+    if (!texture) {
+        return;
+    }
+
+    m_vertex_array->bind();
+    m_vertex_buffer->buffer_data(
+        static_cast<u32>(sizeof(Sprite_Batch_Vertex) * m_current_vertices.size()), m_current_vertices.data()
+    );
+    // m_index_buffer->draw_elements(
+    //     static_cast<i32>((m_current_vertices.size() / QUAD_VERTEX_COUNT) * QUAD_INDEX_COUNT), Draw_Mode::Triangles
+    // );
+
+    m_vertex_array->unbind();
+    m_current_vertices.clear();
 }
 
 }

f142b2b91588bf8ff3bf50e1e6c1f484d0c949af
Rob Graham <r.ggraham@hotmail.com>
2023-04-30 16:00:32 +0100

    chore(ascension)[input]: create input type enums

---
+++ ascension/include/ascension/input/input_types.hpp
@@ -0,0 +1,177 @@
+/**
+ * File: input_types.hpp
+ * Project: ascension
+ * File Created: 2023-04-30 15:34:38
+ * Author: Rob Graham (robgrahamdev@gmail.com)
+ * Last Modified: 2023-04-30 15:44:16
+ * ------------------
+ * Copyright 2023 Rob Graham
+ * ==================
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * ==================
+ */
+
+#pragma once
+
+namespace ascension::input {
+
+enum class Mouse_Button : u32 {
+    LEFT,
+    RIGHT,
+    MIDDLE,
+    MAX_BUTTONS
+};
+
+enum class Key : u32 {
+    UNKNOWN = 0,
+
+    A = 4,
+    B = 5,
+    C = 6,
+    D = 7,
+    E = 8,
+    F = 9,
+    G = 10,
+    H = 11,
+    I = 12,
+    J = 13,
+    K = 14,
+    L = 15,
+    M = 16,
+    N = 17,
+    O = 18,
+    P = 19,
+    Q = 20,
+    R = 21,
+    S = 22,
+    T = 23,
+    U = 24,
+    V = 25,
+    W = 26,
+    X = 27,
+    Y = 28,
+    Z = 29,
+
+    NUM_1 = 30,
+    NUM_2 = 31,
+    NUM_3 = 32,
+    NUM_4 = 33,
+    NUM_5 = 34,
+    NUM_6 = 35,
+    NUM_7 = 36,
+    NUM_8 = 37,
+    NUM_9 = 38,
+    NUM_0 = 39,
+
+    RETURN = 40,
+    ESCAPE = 41,
+    BACKSPACE = 42,
+    TAB = 43,
+    SPACE = 44,
+
+    MINUS = 45,
+    EQUALS = 46,
+    LEFTBRACKET = 47,
+    RIGHTBRACKET = 48,
+    BACKSLASH = 49,
+    NONUSHASH = 50,
+    SEMICOLON = 51,
+    APOSTROPHE = 52,
+    GRAVE = 53,
+    COMMA = 54,
+    PERIOD = 55,
+    SLASH = 56,
+
+    CAPSLOCK = 57,
+
+    F1 = 58,
+    F2 = 59,
+    F3 = 60,
+    F4 = 61,
+    F5 = 62,
+    F6 = 63,
+    F7 = 64,
+    F8 = 65,
+    F9 = 66,
+    F10 = 67,
+    F11 = 68,
+    F12 = 69,
+
+    PRINTSCREEN = 70,
+    SCROLLLOCK = 71,
+    PAUSE = 72,
+    INSERT = 73,
+    HOME = 74,
+    PAGEUP = 75,
+    DELETE = 76,
+    END = 77,
+    PAGEDOWN = 78,
+    RIGHT = 79,
+    LEFT = 80,
+    DOWN = 81,
+    UP = 82,
+
+    NUMLOCKCLEAR = 83,
+
+    KP_DIVIDE = 84,
+    KP_MULTIPLY = 85,
+    KP_MINUS = 86,
+    KP_PLUS = 87,
+    KP_ENTER = 88,
+    KP_1 = 89,
+    KP_2 = 90,
+    KP_3 = 91,
+    KP_4 = 92,
+    KP_5 = 93,
+    KP_6 = 94,
+    KP_7 = 95,
+    KP_8 = 96,
+    KP_9 = 97,
+    KP_0 = 98,
+    KP_PERIOD = 99,
+    KP_EQUALS = 103,
+
+    NONUSBACKSLASH = 100,
+
+    F13 = 104,
+    F14 = 105,
+    F15 = 106,
+    F16 = 107,
+    F17 = 108,
+    F18 = 109,
+    F19 = 110,
+    F20 = 111,
+    F21 = 112,
+    F22 = 113,
+    F23 = 114,
+    F24 = 115,
+
+    VOLUMEUP = 128,
+    VOLUMEDOWN = 129,
+
+    LCTRL = 224,
+    LSHIFT = 225,
+    LALT = 226,
+    LMETA = 227,
+    RCTRL = 228,
+    RSHIFT = 229,
+    RALT = 230,
+    RMETA = 231,
+
+    MODE = 257,
+
+    MAX_KEYS = 512
+};
+
+}
